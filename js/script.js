11121d0a0e91da5f7db3dfc34c6252135ebdc304
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-05 18:28:05 +0000

    added css and js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Nathan De Silva All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Nathan De Silva
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates volume of a rectangular prism
+ */
+function doMathClicked() {
+  // input
+  const length = parseInt(document.getElementById("length").value)
+  const width = parseInt(document.getElementById("width").value)
+  const height = parseInt(document.getElementById("height").value)
+
+  // process
+  const volumeOfPrism = length * width * height
+
+  // output
+  document.getElementById("volume-rectangle").innerHTML =
+    "<p>The volume of a rectangular prism is: " + volumeOfPrism + "cm³. </p>"
+}
